b6978efc947a5d372dd33cd553671fabf870d58e
TI <TI@DESKTOP-0287RFU>
2019-06-03 14:16:54 -0300

    segundocommit

--- testecochea/src/testecochea/Testecochea.java
+++ testecochea/src/testecochea/Testecochea.java
@@ -15,7 +15,7 @@ public class Testecochea {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Palmeiras não tem Mundial");
     }
     
 }
